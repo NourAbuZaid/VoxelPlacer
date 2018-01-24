a43777baf2f13b8d6c93d316b5c7c005643ead2a
Karim <karimdaw91@gmail.com>
2018-01-24 13:54:16 +0100

    Revert "Merge pull request #1 from NourAbuZaid/function_tests"
    This reverts commit 0887a4d7ea94e51ed8347095f573cfc0ecf18ea6, reversing
    changes made to 6cf9107495bc34ad79460eac277c1c5e3cb0e6e3.

--- Person.py
+++ Person.py
@@ -6,9 +6,6 @@ from New_Patterns_Dictionary import Activity_Pattern
 # adding a comment to checo how github works
 # adding a second comment
 #second test
-#[apoisdkap[oskda[poskdap]osdkaposkdap[oskd]]]
-#poaijsdopajwpoiajd
-#try agian
 ####################################################
 
 
