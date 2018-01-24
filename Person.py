b19939c127a2f8649fc038942cff9f342513df3f
Nour Abu Zaid <nourabuzaid@wlan-145-94-213-183.wlan.tudelft.nl>
2018-01-24 11:59:41 +0100

    test comment

--- Person.py
+++ Person.py
@@ -3,7 +3,7 @@ from Schedule import people_dictionary
 
 from New_Patterns_Dictionary import Activity_Pattern
 
-
+# adding a comment to checo how github works 
 
 ####################################################
 
