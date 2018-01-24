a95debdfe506771d1f2a20ad0cd145c485489a0c
Karim <karimdaw91@gmail.com>
2018-01-24 12:26:39 +0100

    lets try this again

--- Person.py
+++ Person.py
@@ -4,7 +4,8 @@ from Schedule import people_dictionary
 from New_Patterns_Dictionary import Activity_Pattern
 
 # adding a comment to checo how github works
-# adding a second comment 
+# adding a second comment
+#second test
 ####################################################
 
 
